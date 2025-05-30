813d50c9d68d8f19dbe1076cd82214a402eca135
Emman Mlmb <emmanjibi@gmail.com>
2025-05-30 19:15:08 +0100

    Update data.js

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { IncomingForm } from 'formidable';
+import formidable from 'formidable';
 import Papa from 'papaparse';
 
 export const config = {
